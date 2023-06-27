2237dec0eb03a9bcbf5bf00f90b3610a02122cdf
ChunYingChou <chunying921015@gmail.com>
2023-06-27 13:57:01 +0800

    修改細節

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -74,7 +74,7 @@
         </p>
       </div>
 
-      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy;2023</p>
     </form>
   </div>
 </template>
